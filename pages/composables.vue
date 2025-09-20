aab0d62f378b387274ec508576800a34f558edc3
louis Gonzalez <gonzalezlouis1981@gmail.com>
2025-09-20 19:37:49 +0200

    ADD useCounter

---
+++ pages/composables.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import { useUserForm } from '~/composables/useUserForm';
+/// counter composable
+// const { count, increment, decrement, reset } = useCounter(5);
+
+const { t } = useI18n();
+
+const {
+  lastname,
+  firstname,
+  email,
+  comment,
+  handleSubmit,
+  rules,
+  firstnameColor,
+  lastnameColor,
+  emailColor,
+} = useUserForm();
+</script>
+
+<template>
+  <!--  <section id="counter-composable">-->
+  <!--    <div class="p-4">-->
+  <!--      <h1>Counter Section</h1>-->
+  <!--      <p>Current value : {{ count }}</p>-->
+
+  <!--      <div class="d-flex align-center ma-1 pa-2 ga-2">-->
+  <!--        <v-btn @click="increment">➕ 1</v-btn>-->
+  <!--        <v-btn @click="decrement">➖ 1</v-btn>-->
+  <!--        <v-btn @click="reset">🔄 Reset</v-btn>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--  </section>-->
+
+  <!--  :rules="['required','notEmpty','string','longerThan6']"-->
+
+  <!--  :rules="['required','notEmpty','string','longerThan6']"-->
+
+  <!--  :rules="['required','notEmpty','string','longerThan6', 'email']"-->
+  <section>
+    <v-form class="ma-4" @submit.prevent="handleSubmit">
+      <v-card border elevation="2" class="ma-2">
+        <h1 class="text-center">{{ t('form_user_title') }}</h1>
+        <v-row class="d-flex ma-2 align-center">
+          <v-col cols="4">
+            <v-text-field
+              v-model="lastname"
+              :color="lastnameColor"
+              :label="t('lastname')"
+              :rules="rules.lastname"
+              variant="outlined"
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="firstname"
+              :color="firstnameColor"
+              :label="t('firstname')"
+              :rules="rules.firstname"
+              variant="outlined"
+            />
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="email"
+              :color="emailColor"
+              :label="t('email')"
+              :rules="rules.email"
+              variant="outlined"
+            />
+          </v-col>
+        </v-row>
+        <v-row class="d-flex ma-2 align-center">
+          <v-col cols="4">
+            <v-text-field
+              v-model="comment"
+              :label="t('comment')"
+              :rules="rules.comment"
+              variant="outlined"
+            />
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col class="d-flex align-center ga-2 justify-center mb-4">
+            <v-btn color="error" outlined>{{ t('cancel') }}</v-btn>
+            <v-btn color="success" type="submit" outlined>{{
+                t('confirm')
+              }}</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-form>
+  </section>
+</template>
+
+<style scoped></style>
